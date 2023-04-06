5eff38dccff40a9cc2d6836fb591044f55055242
Nurlan988 <aliyev.g.nurlan@gmail.com>
2023-04-06 17:20:43 +0400

    first

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
   <div class="app">
     <header>
       <div class="title">
-        <h1>Remote work</h1>
+        <h1>Job search</h1>
       </div>
       <div class="order">
-        <button @click="handleClick('title')">order by title</button>
-        <button @click="handleClick('salary')">order by salary</button>
-        <button @click="handleClick('location')">order by location</button>
+        <button @click="handleClick('title')">Order by Title</button>
+        <button @click="handleClick('salary')">Order by Salary</button>
+        <button @click="handleClick('location')">Order by Location</button>
       </div>
     </header>
     <job-list :jobs="jobs" :order="order"></job-list>
@@ -15,31 +15,31 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import JobList from './components/JobList.vue'
-import Job from './types/Job'
-import OrderTerm from './types/OrderTerm'
+import { defineComponent, ref } from "vue";
+import JobList from "./components/JobList.vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
-      { title: 'Frontend Vue.js', location: 'London', salary: 3000, id: '1' },
-      { title: 'Frontend React', location: 'New York', salary: 4000, id: '2' },
-      { title: 'Backend Java', location: 'Moscow', salary: 3500, id: '3' },
-      { title: 'Backend PHP', location: 'Baku', salary: 2100, id: '4' },
-      { title: 'UI/UX Designer', location: 'Berlin', salary: 3200, id: '5' }
-    ])
-    const order = ref<OrderTerm>('title')
+      { title: "Frontend Vue.js", location: "London", salary: 3000, id: "1" },
+      { title: "Frontend React", location: "New York", salary: 4000, id: "2" },
+      { title: "Backend Java", location: "Moscow", salary: 3500, id: "3" },
+      { title: "Backend PHP", location: "Baku", salary: 2100, id: "4" },
+      { title: "UI/UX Designer", location: "Berlin", salary: 3200, id: "5" },
+    ]);
+    const order = ref<OrderTerm>("title");
 
     const handleClick = (term: OrderTerm) => {
-      order.value = term
-    }
+      order.value = term;
+    };
 
     return { jobs, handleClick, order };
-  }
-})
+  },
+});
 </script>
 
 <style>
